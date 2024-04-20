27e00d4c19c3c8048e575a12ba0bef495c8e4b6a
Marcin Izdebski <mizdebski123@gmail.com>
2024-04-20 12:36:41 +0200

    Styled components added

---
+++ src/core/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+    color: {
+        mainColor: "#EFEEEE",
+        secondColor: "#E9E9E9",
+        fontColor: "#F39A13"
+    },
+
+    breakPoint: {
+        firstBreakPoint: 1380,
+        secondBreakPoint: 1200,
+        mobileMax: 767,
+    }
+};
