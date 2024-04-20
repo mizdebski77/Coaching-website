452a56e0ffe10c534215706546086f3ff34cb5c1
Marcin Izdebski <mizdebski123@gmail.com>
2024-04-20 12:50:49 +0200

    Navigation added

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { Navbar } from '../common/Navbar/navbar';
 
 function App() {
   return (
-    null
+    <BrowserRouter>
+      <Navbar />
+
+    </BrowserRouter>
   );
 }
 
